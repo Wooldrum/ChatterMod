7afcc19e2e5c7cdf63e98e3644414431f4fe4cf7
Wooldrum <109412071+Wooldrum@users.noreply.github.com>
2025-06-08 17:39:40 -0400

    Update build.gradle.kts

---
+++ ChatterMod/build.gradle.kts
@@ -0,0 +1,52 @@
+// File: build.gradle.kts
+plugins {
+    // Fabric Loom plugin for Minecraft modding
+    id("fabric-loom") version "1.3.5"
+    id("maven-publish")
+}
+
+group = "com.example.youtubechat"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.fabricmc.net/") }
+}
+
+dependencies {
+    // Minecraft & Yarn mappings
+    minecraft("com.mojang:minecraft:1.21.5")
+    mappings("net.fabricmc:yarn:1.21.5+build.5:v2")
+    
+    // Fabric loader & Fabric API
+    modImplementation("net.fabricmc:fabric-loader:0.17.3")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.88.0+1.21.5")
+
+    // GSON (for parsing YouTube JSON responses)
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+java {
+    // We target Java 17 (Minecraft 1.21.5 runs on Java 17+)
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+        }
+    }
+}
