b2a689427077b7fe673be49faddf969926873eba
Wooldrum <109412071+Wooldrum@users.noreply.github.com>
2025-06-08 23:58:30 -0400

    Update build.gradle.kts

--- ChatterMod/build.gradle.kts
+++ ChatterMod/build.gradle.kts
@@ -4,23 +4,22 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-// --- (The rest of the file is mostly the same) ---
-
 val minecraft_version = "1.21.5"
 val fabric_loader_version = "0.16.14"
 val fabric_api_version = "0.126.0+1.21.5"
 val yarn_mappings = "1.21.5+build.1:v2"
 val gson_version = "2.10.1"
 val twitch4j_version = "1.20.0"
-val kick_api_version = "1.1.2" // Community API for Kick (CHANGED to working version)
+// NEW, WORKING KICK LIBRARY VERSION
+val kick_java_version = "1.4.0"
 
 group = "com.wooldrum"
-version = "2.0.0"
+version = "2.1.0" // Version bump for the new library
 
 repositories {
-    mavenCentral()
+    mavenCentral() // The new Kick library is here
     maven { url = uri("https://maven.fabricmc.net/") }
-    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://jitpack.io") } // Still needed for Twitch4J
 }
 
 dependencies {
@@ -30,19 +29,16 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:$fabric_loader_version")
     modImplementation("net.fabricmc.fabric-api:fabric-api:$fabric_api_version")
 
-    // GSON for JSON parsing
     implementation("com.google.code.gson:gson:$gson_version")
 
-    // Twitch4J for Twitch Integration
-    // Shadow plugin will bundle these into the final JAR
+    // Twitch4J (unchanged)
     implementation("com.github.twitch4j:twitch4j-chat:$twitch4j_version")
     implementation("com.github.twitch4j:twitch4j-auth:$twitch4j_version")
     implementation("com.github.twitch4j:twitch4j-common:$twitch4j_version")
 
-    // Java-Kick-API for Kick Integration
-    implementation("com.github.jroy:Java-Kick-API:$kick_api_version")
+    // REMOVED old Kick API, ADDED new one
+    implementation("io.github.oxylca:kick-java:$kick_java_version")
 
-    // SLF4J is required by Twitch4J
     implementation("org.slf4j:slf4j-simple:2.0.13")
 }
 
@@ -58,11 +54,8 @@ tasks.processResources {
     }
 }
 
-// The shadow plugin creates a new 'shadowJar' task.
-// We configure it to be the main artifact.
 tasks.shadowJar {
-    archiveClassifier.set("all") // Appends "-all" to the file name
-    // Merge service files required by Twitch4J
+    archiveClassifier.set("all")
     mergeServiceFiles()
 }
 
@@ -70,11 +63,9 @@ tasks.build {
     dependsOn(tasks.shadowJar)
 }
 
-// Publishing configuration (optional, but good practice)
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
-            // Use the output of the shadowJar task for publishing
             artifact(tasks.shadowJar)
         }
     }
