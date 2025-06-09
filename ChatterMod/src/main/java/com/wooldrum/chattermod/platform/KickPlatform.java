1d257c9664d04d8a027aa6412f33ea37f76c6543
Wooldrum <109412071+Wooldrum@users.noreply.github.com>
2025-06-08 23:58:57 -0400

    Update KickPlatform.java

--- ChatterMod/src/main/java/com/wooldrum/chattermod/platform/KickPlatform.java
+++ ChatterMod/src/main/java/com/wooldrum/chattermod/platform/KickPlatform.java
@@ -1,48 +1,69 @@
 package com.wooldrum.chattermod.platform;
 
-import com.jroy.kick.KickAPI;
-import com.jroy.kick.events.ChatMessageEvent;
 import com.wooldrum.chattermod.ChatterMod;
 import com.wooldrum.chattermod.ChatterModConfig;
+import io.github.oxylca.kick.KickApi;
+import io.github.oxylca.kick.KickListener;
+import io.github.oxylca.kick.models.ChatMessage;
 
 import java.util.function.Consumer;
 
 public class KickPlatform implements ChatPlatform {
 
     private final ChatterModConfig.KickAccount account;
-    private KickAPI kickAPI;
-    private Consumer<ChatMessage> messageConsumer;
+    private KickApi kickApi;
+    private Consumer<com.wooldrum.chattermod.platform.ChatMessage> messageConsumer;
 
     public KickPlatform(ChatterModConfig.KickAccount account) {
         this.account = account;
     }
 
     @Override
-    public void onMessage(Consumer<ChatMessage> consumer) {
+    public void onMessage(Consumer<com.wooldrum.chattermod.platform.ChatMessage> consumer) {
         this.messageConsumer = consumer;
     }
 
     @Override
     public void connect() {
         try {
-            kickAPI = KickAPI.builder().channel(account.channelName()).build();
-            kickAPI.connectBlocking();
+            // The new library uses a builder pattern and a listener
+            kickApi = KickApi.builder().channel(account.channelName()).build();
 
-            kickAPI.getEventManager().onEvent(ChatMessageEvent.class, event -> {
-                String author = event.getSender().getUsername();
-                String message = event.getContent();
-                messageConsumer.accept(new ChatMessage(author, message, ChatMessage.Platform.KICK));
+            kickApi.registerListener(new KickListener() {
+                @Override
+                public void onChatMessage(io.github.oxylca.kick.models.ChatMessage event) {
+                    // Convert the library's message object to our universal ChatMessage
+                    String author = event.getSender().getUsername();
+                    String message = event.getMessage();
+                    messageConsumer.accept(new com.wooldrum.chattermod.platform.ChatMessage(author, message, com.wooldrum.chattermod.platform.ChatMessage.Platform.KICK));
+                }
+
+                @Override
+                public void onConnected() {
+                    ChatterMod.LOGGER.info("[Kick] Connected to channel: {}", account.channelName());
+                }
+
+                @Override
+                public void onClose(int code, String reason) {
+                    ChatterMod.LOGGER.warn("[Kick] Connection closed: {} - {}", code, reason);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    ChatterMod.LOGGER.error("[Kick] Connection failure for channel '{}'", account.channelName(), t);
+                }
             });
-            ChatterMod.LOGGER.info("[Kick] Connected to channel: {}", account.channelName());
+
+            kickApi.connect(); // This is non-blocking
         } catch (Exception e) {
-            ChatterMod.LOGGER.error("[Kick] Failed to connect to channel '{}'. Is the name correct?", account.channelName(), e);
+            ChatterMod.LOGGER.error("[Kick] Failed to build API for channel '{}'", account.channelName(), e);
         }
     }
 
     @Override
     public void disconnect() {
-        if (kickAPI != null) {
-            kickAPI.close();
+        if (kickApi != null) {
+            kickApi.close();
             ChatterMod.LOGGER.info("[Kick] Disconnected from channel: {}", account.channelName());
         }
     }
