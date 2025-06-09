8c1a1d9ccd9d0fd11cc1be96aeee4957fcc1816a
Wooldrum <109412071+Wooldrum@users.noreply.github.com>
2025-06-08 22:39:22 -0400

    Update Config.java

--- ChatterMod/src/main/java/com/wooldrum/chattermod/Config.java
+++ ChatterMod/src/main/java/com/wooldrum/chattermod/Config.java
@@ -1,19 +1,1 @@
-package com.wooldrum.chattermod;
 
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
-
-/**
- * Your mod’s runtime configuration.
- * Any public fields here will become options in-game.
- */
-@Config(name = "chattermod")
-public class Config {
-    /**
-     * If true, debug info (e.g. raw YouTube chat JSON) will be logged.
-     */
-    @ConfigEntry.Gui.Tooltip
-    public boolean debug = false;
-
-    // → add more options here
-}
