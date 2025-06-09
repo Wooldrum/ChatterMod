81e3d615d93d4543475aeb416d7065445c268db8
Wooldrum <109412071+Wooldrum@users.noreply.github.com>
2025-06-08 22:35:15 -0400

    Update ChatterModConfig.java

--- ChatterMod/src/main/java/com/wooldrum/chattermod/ChatterModConfig.java
+++ ChatterMod/src/main/java/com/wooldrum/chattermod/ChatterModConfig.java
@@ -1,11 +1,20 @@
+// src/main/java/com/wooldrum/chattermod/ChatterModConfig.java
+
 package com.wooldrum.chattermod;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 
+/**
+ * Handles registration and loading of our config file.
+ * Call ChatterModConfig.init() once in your mod initializer
+ * before accessing Config.INSTANCE.
+ */
 public class ChatterModConfig {
+    /** The loaded config instance. */
     public static Config INSTANCE;
 
+    /** Register the Config class and load the instance. */
     public static void init() {
         AutoConfig.register(Config.class, JanksonConfigSerializer::new);
         INSTANCE = AutoConfig.getConfigHolder(Config.class).getConfig();
